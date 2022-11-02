4f41472b6c343bc91571352034ab6c1502a9cccb
lisa-go <lis-zaa@live.com>
2022-11-02 01:45:56 -0500

    created class for task and working form to get task info to create new tasks

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
 import "./style.scss";
+
+
+let myTasks = [];
+
+class Task {
+    constructor(title, description, dueDate, priority) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    }
+        addToList(){ 
+            myTasks.push([this.title, this.description, this.dueDate, this.priority]);
+            return this;
+        }
+}
+
+//* event listener for submit button on form *//
+const formBtn = document.querySelector('#formBtn');
+formBtn.addEventListener("click", newTask);
+
+//* create a new task with form values *//
+function newTask() {
+    let title = document.querySelector("#title").value;
+    let description = document.querySelector("#description").value;
+    let dueDate = document.querySelector("#dueDate").value;
+    let priority = document.querySelector("#priority").value;
+
+    var task = new Task(title, description, dueDate, priority);
+
+    task.addToList();
+    console.log(myTasks);
+}
+
+
+
