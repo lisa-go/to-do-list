cecb36cf80e6ebebc4abea202e8ba65a3746e100
lisa-go <lis-zaa@live.com>
2022-11-03 01:58:19 -0500

    new projects show on sidebar and form, tasks show up on container

--- src/display.js
+++ src/display.js
@@ -1,5 +1,79 @@
-function displayTasks() {
-    console.log ('hi');
+import { myProjects, myTasks } from "./index.js";
+
+    const taskCont = document.querySelector('#taskContainer');
+export function newdisplayTask() {
+    //* container for task *//
+    const taskDisplay = document.createElement('task');
+    taskDisplay.classList.add('task' + myTasks.length);
+    taskDisplay.dataset.id = myTasks.slice(-1)[0][4];
+    taskCont.appendChild(taskDisplay);
+
+        const taskD5 = document.createElement('priority');
+        taskD5.classList.add('task' + myTasks.length);
+        taskD5.textContent = myTasks.slice(-1)[0][3];
+        taskDisplay.appendChild(taskD5);
+
+        const taskD1 = document.createElement('input');
+        taskD1.type = 'checkbox';
+        taskD1.classList.add('task' + myTasks.length);
+        taskD1.classList.add('checkbox');
+        taskDisplay.appendChild(taskD1);
+
+        const taskD2 = document.createElement('ptitle');
+        taskD2.classList.add('task' + myTasks.length);
+        taskD2.textContent = myTasks.slice(-1)[0][0];
+        taskDisplay.appendChild(taskD2);
+
+        const taskD3 = document.createElement('description');
+        taskD3.classList.add('task' + myTasks.length);
+        taskD3.textContent = myTasks.slice(-1)[0][1];
+        taskDisplay.appendChild(taskD3);
+
+        const taskD4 = document.createElement('dueDate');
+        taskD4.classList.add('task' + myTasks.length);
+        taskD4.textContent = myTasks.slice(-1)[0][2];
+        taskDisplay.appendChild(taskD4);
+
+        const taskD6 = document.createElement('project');
+        taskD6.classList.add('task' + myTasks.length);
+        taskD6.textContent = myTasks.slice(-1)[0][4];
+        taskDisplay.appendChild(taskD6);
+
+        const taskD7 = document.createElement('settings');
+        taskD7.classList.add('task' + myTasks.length);
+        taskD7.textContent = 'temp settings';
+        taskDisplay.appendChild(taskD7);
 }
 
-export default displayTasks;
+export function displayTasks() {
+    
+    console.log ('hi');
+    
+}
+
+
+//* create new divs for new projects *//
+    const projectCont = document.querySelector('.project-list');
+export function displayProjects() {
+    const newProject = document.createElement('button');
+    newProject.id = myProjects.slice(-1);
+    newProject.textContent = myProjects.slice(-1);
+    newProject.onclick = displayTasks;
+    projectCont.appendChild(newProject);
+}
+
+
+//* default project displays *//
+export function displayDprojects() {
+    const allProjects = document.createElement('button');
+    allProjects.id = 'projAll';
+    allProjects.textContent = 'All';
+    allProjects.onclick = displayTasks; //*change later to display all projects *//
+    projectCont.appendChild(allProjects);
+
+    const defaultP = document.createElement('button');
+    defaultP.id = 'default';
+    defaultP.textContent = myProjects.slice(0);
+    defaultP.onclick = displayTasks;
+    projectCont.appendChild(defaultP)
+}
--- src/index.js
+++ src/index.js
@@ -1,20 +1,48 @@
 import "./style.scss";
-import displayTasks from './display.js';
+import { displayTasks, displayProjects, displayDprojects, newdisplayTask } from './display.js';
+import logoIcon from './images/edit.svg';
+import addIcon from './images/add.svg';
 
-let myTasks = [];
+export let myTasks = [];
 
 class Task {
-    constructor(title, description, dueDate, priority) {
+    constructor(title, description, dueDate, priority, project) {
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
     this.priority = priority;
+    this.project = project;
     }
         addToList(){ 
-            myTasks.push([this.title, this.description, this.dueDate, this.priority]);
+            myTasks.push([this.title, this.description, this.dueDate, this.priority, this.project]);
             return this;
         }
 }
+
+class Project {
+    constructor(pname) {
+        this.pname = pname;
+    }
+        addToProjectlist() {
+            myProjects.push(this.pname);
+            return this;
+        }
+}
+
+//* adding images *//
+const logo = new Image();
+logo.src = logoIcon;
+logo.classList.add('filter-brown');
+document.querySelector('.header').appendChild(logo);
+
+const add = new Image();
+add.src = addIcon;
+add.classList.add('resize');
+document.querySelector('#openForm').appendChild(add);
+const add2 = new Image();
+add2.src = addIcon;
+add2.classList.add('resize');
+document.querySelector('#addProject').appendChild(add2);
 
 
 //* event listener for submit button on form *//
@@ -27,13 +55,14 @@ function newTask() {
     let description = document.querySelector("#description").value;
     let dueDate = document.querySelector("#dueDate").value;
     let priority = document.querySelector("#priority").value;
+    let project = document.querySelector("#project").value;
 
-    var task = new Task(title, description, dueDate, priority);
+    var task = new Task(title, description, dueDate, priority, project);
 
     task.addToList();
 
     closeForm();
-    displayTasks();
+    newdisplayTask();
     console.log(myTasks);
 }
 
@@ -50,8 +79,58 @@ function openForm() {
 //* event listener for cancel button on form *//
 const closeFormBtn = document.querySelector('#closeForm');
 closeFormBtn.addEventListener("click", closeForm);
+const closeFormBtn2 = document.querySelector('#closeForm2');
+closeFormBtn2.addEventListener("click", closeForm);
 
 //* function to open form *//
 function closeForm() {
     document.querySelector('#formBg').style.display = "none";
+    document.querySelector('#formBg2').style.display = "none";
 }
+
+//* project list on form *//
+const projectListCont = document.querySelector('.project');
+const projectList = document.createElement('select');
+projectList.name = 'project';
+projectList.id = 'project';
+projectList.required = 'true';
+projectListCont.appendChild(projectList);
+
+const defaultProject = document.createElement('option');
+defaultProject.value = 'default';
+defaultProject.textContent = 'Default';
+defaultProject.selected = 'true';
+projectList.appendChild(defaultProject);
+
+export let myProjects = [projectList.value];
+
+        //* event listener for opening add project form *//
+        const addProjectBtn = document.querySelector('#addProject');
+        addProjectBtn.addEventListener("click", openForm2);
+
+        function openForm2() {
+            document.querySelector('#formBg2').style.display = "flex";
+        }
+
+        //* add new projects *//
+        const formBtn2 = document.querySelector('#formBtn2');
+        formBtn2.addEventListener("click", newProject);
+
+        function newProject() {
+            let pname = document.querySelector("#newProject").value;
+            var project = new Project(pname);
+            project.addToProjectlist();
+            
+            let projectName = myProjects.slice(-1);
+            const projectStr = document.createElement('option');
+            projectStr.value = projectName;
+            projectStr.textContent = projectName;
+            projectList.appendChild(projectStr);
+
+            closeForm();
+            displayProjects();
+            console.log (myProjects);
+        }
+
+        displayDprojects();
+
